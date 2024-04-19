a5bc3b1afb7ebaed93345916edc81f1e224997bc
seunghyun <inyo0506@gmail.com>
2024-04-19 16:22:21 +0900

    cra없이 react-ts세팅

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>되는건가</div>
+    </>
+  );
+};
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
